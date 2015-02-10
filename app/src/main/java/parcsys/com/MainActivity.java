5d09e209ed715126cd3088eed787b77e56a0d336
Ivan Melnikov <ivan9105@bk.ru>
2015-02-11 00:54:03 +0400

    added action bar and action-cap shopping card

--- app/src/main/java/parcsys/com/MainActivity.java
+++ app/src/main/java/parcsys/com/MainActivity.java
@@ -1,9 +1,14 @@
 package parcsys.com;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewConfiguration;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
 
 import parcsys.com.marketfinal.R;
 
@@ -16,25 +21,46 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        /*ActionBar actionBar = getSupportActionBar();
+        actionBar.setSubtitle("mytest");
+        actionBar.setTitle("vogella.com");
+        actionBar.setIcon(null);
+        actionBar.show();
+        getOverflowMenu();*/
     }
 
+    private void getOverflowMenu() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKeyField = ViewConfiguration.class.getDeclaredField("sHasPermanentMenuKey");
+            if(menuKeyField != null) {
+                menuKeyField.setAccessible(true);
+                menuKeyField.setBoolean(config, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.actions_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.card:
+                Toast.makeText(this, "card", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+            default:
+                break;
         }
-        return super.onOptionsItemSelected(item);
+
+        return true;
     }
 }
